be7669557152aaefe27c6431d97ff530145d9b86
Sergii.Kliuchnyk <sergii.kliuchnyk@masterofcode.com>
2020-11-08 09:12:16 +0200

    homework 3 - dom

---
+++ dom/index.js
@@ -0,0 +1,43 @@
+jQuery('input').on('paste', function (e) {
+	e.preventDefault();
+
+	var text = e.originalEvent.clipboardData.getData('text/plain');
+	var input = e.currentTarget;
+
+
+});
+
+jQuery('th').on('contextmenu', function (e) {
+	e.preventDefault();
+
+	var menu = jQuery('#column-menu');
+
+	menu.addClass('d-block');
+
+	menu.css({
+		left: e.clientX,
+		top: e.clientY
+	});
+});
+
+jQuery('#column-menu [data-action]').on('click', function (e) {
+	e.preventDefault();
+
+	var action = e.currentTarget.getAttribute('data-action');
+
+	switch (action) {
+		case 'add-left':
+
+			break;
+
+		case 'add-right':
+
+			break;
+
+		case 'remove':
+
+			break;
+	}
+
+	jQuery('#column-menu').removeClass('d-block');
+});
